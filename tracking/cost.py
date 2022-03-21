47723d828c2416cd4ee669a43c9331970ad04c5b
Ikochoy <kwan.choy@mail.utoronto.ca>
2022-03-21 13:02:27 -0400

    add helper function adn completed iou_2d, push for testing

--- tracking/cost.py
+++ tracking/cost.py
@@ -2,6 +2,17 @@ import numpy as np
 from shapely.geometry import Polygon
 
 
+def get_params(info):
+    centroid_x, centroid_y = info[0], info[1]
+    box_x, box_y = info[2], info[3]
+    heading = info[-1]
+    # before_rotation matrix should be 2 x 4 array
+    before_rotation = np.array([[centroid_x-box_x/2, centroid_y-box_y/2], [centroid_x-box_x/2, centroid_y+box_y/2],
+        [centroid_x+box_x/2, centroid_y+box_y/2], [centroid_x+box_x/2, centroid_y-box_y/2]]).T
+    rotation_matrix = np.array([[np.cos(heading), -np.sin(heading)], [np.sin(heading), np.cos(heading)]])
+    after_rotation = np.matmul(rotation_matrix, before_rotation).T
+    return after_rotation.tolist()
+
 def iou_2d(bboxes1: np.ndarray, bboxes2: np.ndarray) -> np.ndarray:
     """Computes 2D intersection over union of two sets of bounding boxes
 
@@ -15,4 +26,14 @@ def iou_2d(bboxes1: np.ndarray, bboxes2: np.ndarray) -> np.ndarray:
     M, N = bboxes1.shape[0], bboxes2.shape[0]
     # TODO: Replace this stub code.
     iou_mat = np.zeros((M, N))
+    for i in range(M):
+        for j in range(N):
+            polygon1params = get_params(bboxes1[i])
+            polygon2params = get_params(bboxes2[j])
+            polygon1 = Polygon(polygon1params)
+            polygon2 = Polygon(polygon2params)
+            intersect = polygon1.intersection(polygon2).area
+            union = polygon1.union(polygon2).area
+            iou = intersect / union
+            iou_mat[i][j] = iou
     return iou_mat
